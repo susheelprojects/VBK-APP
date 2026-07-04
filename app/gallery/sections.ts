2301ce45b61b508ea4273f8233e69f2c1c8994bf
VBK-BJP <susheel@local.dev>
2026-07-03 20:49:32 -0400

    foldedr

--- app/gallery/sections.ts
+++ app/gallery/sections.ts
@@ -6,3 +6,12 @@ export const gallerySections = [
   { title: "Youth Events", folder: "youth-Events" },
   { title: "Election Campaign", folder: "election-campaign"},
 ];
+
+export const gallerySections = [
+  { title: "During Tough Times – Covid", folder: "during-tough-times-covid" },
+  { title: "Cultural Celebrations", folder: "cultural-celebrations" },
+  { title: "Development Works", folder: "development-works" }, // FIXED
+  { title: "Organization Meetings", folder: "organization-meetings" },
+  { title: "Youth Events", folder: "youth-events" }, // FIXED
+  { title: "Election Campaign", folder: "election-campaign" },
+];
